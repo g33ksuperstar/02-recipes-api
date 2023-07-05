e3349b7c4e241bf1cba45d5509c1a0f2870304ae
g33ksuperstar <g33k.superstar@gmail.com>
2023-07-04 21:14:25 -0600

    delete recipe endpoint

--- main.go
+++ main.go
@@ -79,10 +79,31 @@ func UpdateRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipe)
 }
 
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+	for i := 0; i < len(recipeList); i++ {
+		if recipeList[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "recipe not found"})
+		return
+	}
+
+	recipeList = append(recipeList[:index], recipeList[index+1:]...)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe has been deleted"})
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", DeleteRecipeHandler)
 	router.Run()
 }
